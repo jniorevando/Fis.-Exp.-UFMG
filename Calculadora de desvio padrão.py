69ff6ade1f34fd4aa4514d0f1cfdf4a7f32d0ff4
Evando Júnior <118923696+bingolos@users.noreply.github.com>
2022-11-23 14:12:09 -0300

    Add files via upload

---
+++ Calculadora de desvio padrão.py
@@ -0,0 +1,31 @@
+import math
+
+n = int(input("O valor de n:"))
+f = list()
+
+count = 0
+while count < n:
+    x = float(input("Valor:"))
+    f.append(x)
+    count += 1
+
+a = 0   
+for i in f:
+    a += i
+    
+x1= a/(len(f))
+g = list()
+
+for i in f:
+    b = i**2 - 2*i*x1 + x1**2
+    g.append(b)
+
+d = 0
+for i in g:
+    d += i
+    
+Dp = (d/(n-1))**(1/2)
+
+print("Valor médio:", x1)
+print("Desvio padrão:", Dp)
+    
